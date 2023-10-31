639925d040c638720d6f37614e827f33aca527db
RadomirTikkerbar <145449829+RadomirTikkerbar@users.noreply.github.com>
2023-10-31 15:06:30 +0200

    Create countCode.js

---
+++ countCode.js
@@ -0,0 +1,31 @@
+async function countCode(letter) {
+  try {
+    
+    const response = await fetch('https://raw.githubusercontent.com/timotr/progre-helper/main/data.js');
+    
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = await response.json();
+    
+  
+    const count = data.reduce((acc, code) => {
+      if (code.toLowerCase().startsWith(letter.toLowerCase())) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+
+    return count;
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return 0;
+  }
+}
+
+
+countCode('i')
+  .then(count => {
+    console.log(`Number of codes starting with 'i': ${count}`);
+  });
